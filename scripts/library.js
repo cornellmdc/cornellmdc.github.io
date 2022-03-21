206830a35f3d7fae68f6644c4a61e3a216bea426
jason-tung <jt568@cornell.edu>
2022-03-21 15:46:53 -0400

    publication page

--- scripts/library.js
+++ scripts/library.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-    $('.carousel').slick({
-        infinite: true,
-        slidesToShow: 1
-    });
-})
 
-$('.book').click(function() {
+
+
+$('.book').click(function () {
     let firstload = $(".book-selected").length == 0;
     if (firstload) {
-        $(".library-preview").css("display", "flex");
-        $('.carousel').slick('setPosition'); 
+        $(".library-preview").css("display", "block");
+        $('.carousel').slick({
+            infinite: true,
+            slidesToShow: 1,
+            prevArrow: `<img type="button" class="slick-next" src='/img/prev.svg'>`,
+            nextArrow: `<img type="button" class="slick-prev" src='/img/next.svg'>`
+        });
     } else {
         $(".book-selected").removeClass("book-selected");
     }
