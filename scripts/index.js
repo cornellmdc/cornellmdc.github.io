eee3ad982258993108eb95e73fbb24146870f33a
jason-tung <jt568@cornell.edu>
2022-03-29 13:13:30 -0400

    stuff

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,11 @@ const mobileMenuClose = () => {
     const mobileNav = document.querySelector(".overlay-nav");
     mobileNav.style.display = "none";
 }
+
+const enableHeader = (cat) => {
+    $(".nav-holder").load("/templates/header.html", () => {
+        $(cat).removeClass("inactive-link");
+        $(cat).addClass("active-link");
+    });
+    // $("#footer").load("/templates/footer.html");
+}
--- scripts/script.js
+++ scripts/script.js
@@ -45,3 +45,11 @@ menus.forEach(menu => {
 })
 
 
+window.addEventListener('click', ({ target }) => {
+    const popup = target.closest('.popup');
+    const clickedOnClosedPopup = popup && !popup.classList.contains('show');
+
+    popups.forEach(p => p.classList.remove('show'));
+
+    if (clickedOnClosedPopup) popup.classList.add('show');
+});
